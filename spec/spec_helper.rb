e4853f358736dc0a930efc8e7c2516272062329f
Mik Quinlan <mik.quinlan@gmail.com>
2014-04-08 18:22:53 -0700

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Added by Mik for rails tutorial tests
+  config.include Capybara::DSL
 end
